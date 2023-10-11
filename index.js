3d529b6ffe15711e5086d79c1feb9664ffac8675
ingridmidia <ingridmidia@gmail.com>
2023-10-11 18:00:28 -0500

    basic version of readme file is being created

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your project's name?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Please, insert your project's description:",
+        name: "description"
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(`${fileName}.md`, generateMarkdown(data), (err) =>
+        err ? console.error(err) : console.log('README file was created.')
+    )
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then(function (data) {
+            writeToFile("README2", data);
+        });
+}
 
 // Function call to initialize app
 init();
