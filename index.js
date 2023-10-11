d46cd688716ef402ebacd320e5cc6d71482d91af
ingridmidia <ingridmidia@gmail.com>
2023-10-11 18:47:34 -0500

    added more questions

--- index.js
+++ index.js
@@ -14,6 +14,24 @@ const questions = [
         type: "input",
         message: "Please, insert your project's description:",
         name: "description"
+    },
+    {
+        type: "list",
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        default: "npm i",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "What command should be run to run tests?",
+        default: "npm test",
+        name: "test"
     }
 ];
 
